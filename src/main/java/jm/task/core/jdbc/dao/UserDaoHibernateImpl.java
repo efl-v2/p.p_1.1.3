5b45fff907fbe09c9f103bcceeed8d9e5610eca5
efl-v2 <gerfedorov0@gmail.com>
2024-10-24 01:06:21 +0500

    Update UserDaoHibernateImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,166 +1,166 @@
-package jm.task.core.jdbc.dao;
+// package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import java.util.ArrayList;
-import java.util.List;
+// import jm.task.core.jdbc.model.User;
+// import jm.task.core.jdbc.util.Util;
+// import org.hibernate.Session;
+// import org.hibernate.Transaction;
+// import org.hibernate.query.Query;
+// import java.util.ArrayList;
+// import java.util.List;
 
 
-public class UserDaoHibernateImpl implements UserDao {
+// public class UserDaoHibernateImpl implements UserDao {
 
-    public UserDaoHibernateImpl() {
-    }
+//     public UserDaoHibernateImpl() {
+//     }
 
-    @Override
-    public void createUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = null;
-            try {
-                transaction = session.beginTransaction();
+//     @Override
+//     public void createUsersTable() {
+//         try (Session session = Util.getSessionFactory().openSession()) {
+//             Transaction transaction = null;
+//             try {
+//                 transaction = session.beginTransaction();
 
-                String sqlQuery = "CREATE TABLE IF NOT EXISTS users (\n" +
-                        " id BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
-                        " name VARCHAR(255) NOT NULL,\n" +
-                        " lastName VARCHAR(255) NOT NULL,\n" +
-                        " age TINYINT NOT NULL\n" +
-                        " )";
+//                 String sqlQuery = "CREATE TABLE IF NOT EXISTS users (\n" +
+//                         " id BIGINT PRIMARY KEY AUTO_INCREMENT,\n" +
+//                         " name VARCHAR(255) NOT NULL,\n" +
+//                         " lastName VARCHAR(255) NOT NULL,\n" +
+//                         " age TINYINT NOT NULL\n" +
+//                         " )";
 
-                session.createSQLQuery(sqlQuery).executeUpdate();
+//                 session.createSQLQuery(sqlQuery).executeUpdate();
 
-                transaction.commit();
+//                 transaction.commit();
 
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//             } catch (Exception e) {
+//                 if (transaction != null) {
+//                     transaction.rollback();
+//                 }
+//                 e.printStackTrace();
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
 
-    @Override
-    public void dropUsersTable() {
+//     @Override
+//     public void dropUsersTable() {
 
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = null;
-            try {
-                transaction = session.beginTransaction();
+//         try (Session session = Util.getSessionFactory().openSession()) {
+//             Transaction transaction = null;
+//             try {
+//                 transaction = session.beginTransaction();
 
-                String sqlQuery = "DROP TABLE IF EXISTS users";
+//                 String sqlQuery = "DROP TABLE IF EXISTS users";
 
-                session.createSQLQuery(sqlQuery).executeUpdate();
+//                 session.createSQLQuery(sqlQuery).executeUpdate();
 
-                transaction.commit();
+//                 transaction.commit();
 
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//             } catch (Exception e) {
+//                 if (transaction != null) {
+//                     transaction.rollback();
+//                 }
+//                 e.printStackTrace();
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-    }
+//     }
 
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
+//     @Override
+//     public void saveUser(String name, String lastName, byte age) {
 
-        User user = new User(name, lastName, age);
+//         User user = new User(name, lastName, age);
 
-        try (Session session = Util.getSessionFactory().openSession()) {
+//         try (Session session = Util.getSessionFactory().openSession()) {
 
-            Transaction transaction = null;
+//             Transaction transaction = null;
 
-            try {
-                transaction = session.beginTransaction();
+//             try {
+//                 transaction = session.beginTransaction();
 
-                session.save(user);
+//                 session.save(user);
 
-                transaction.commit();
+//                 transaction.commit();
 
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//             } catch (Exception e) {
+//                 if (transaction != null) {
+//                     transaction.rollback();
+//                 }
+//                 e.printStackTrace();
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
 
-    @Override
-    public void removeUserById(long id) {
+//     @Override
+//     public void removeUserById(long id) {
 
-        try (Session session = Util.getSessionFactory().openSession()) {
+//         try (Session session = Util.getSessionFactory().openSession()) {
 
-            Transaction transaction = null;
+//             Transaction transaction = null;
 
-            try {
-                transaction = session.beginTransaction();
+//             try {
+//                 transaction = session.beginTransaction();
 
-                User user = session.get(User.class, id);
+//                 User user = session.get(User.class, id);
 
-                session.delete(user);
+//                 session.delete(user);
 
-                transaction.commit();
+//                 transaction.commit();
 
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace();
-            }
+//             } catch (Exception e) {
+//                 if (transaction != null) {
+//                     transaction.rollback();
+//                 }
+//                 e.printStackTrace();
+//             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-    }
+//     }
 
-    @Override
-    public List<User> getAllUsers() {
+//     @Override
+//     public List<User> getAllUsers() {
 
-        List<User> users = new ArrayList<>();
+//         List<User> users = new ArrayList<>();
 
-        try (Session session = Util.getSessionFactory().openSession()) {
+//         try (Session session = Util.getSessionFactory().openSession()) {
 
-            Query<User> query = session.createQuery("from User", User.class);
+//             Query<User> query = session.createQuery("from User", User.class);
 
-            users = query.getResultList();
+//             users = query.getResultList();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return users;
-    }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//         return users;
+//     }
 
-    @Override
-    public void cleanUsersTable() {
-        try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = null;
-            try {
-                transaction = session.beginTransaction();
+//     @Override
+//     public void cleanUsersTable() {
+//         try (Session session = Util.getSessionFactory().openSession()) {
+//             Transaction transaction = null;
+//             try {
+//                 transaction = session.beginTransaction();
 
-                session.createQuery("delete from User").executeUpdate();
+//                 session.createQuery("delete from User").executeUpdate();
 
-                transaction.commit();
+//                 transaction.commit();
 
-            } catch (Exception e) {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//             } catch (Exception e) {
+//                 if (transaction != null) {
+//                     transaction.rollback();
+//                 }
+//                 e.printStackTrace();
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
